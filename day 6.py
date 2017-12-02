081a216b3adba88c2211c0d1836ae47c5b325283
Austin Lord <ahlord@iu.edu>
2017-12-02 10:53:14 -0500

    add solution for day 6

--- day 6.py
+++ day 6.py
@@ -1,12 +1,43 @@
-from collections import Counter
-puzzleInput = open('day 6 input.txt', 'r').read().split('\n')
-answer = ''
-for i in range(0, len(puzzleInput[0])-1):
-    charInput = [line[i] for line in puzzleInput]
+def most_common(lst):
+    return min(set(lst), key=lst.count)
 
-    charFrequency = Counter(charInput)
-    mostCommon,count = Counter.most_common(1)
-    print(mostCommon)
-    answer = answer + mostCommon
 
+def get_rows(message_input: str):
+    return message_input.split("\n")
+
+
+def common_message(message_input: str):
+    answer = ''
+    messages = get_rows(message_input)
+    for i in range(0, len(messages[0])):
+        charInput = [line[i] for line in messages]
+        mostCommon = most_common(charInput)
+        answer = answer + mostCommon
+
+    return answer
+
+
+puzzleInput = open('day 6 input.txt', 'r').read()
+answer = common_message(puzzleInput)
+print(answer)
+
+
+puzzleInput = '''eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar'''
+
+answer = common_message(puzzleInput)
 print(answer)
